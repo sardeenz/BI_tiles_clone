1a2491a276ad8c26064e16361a5a989e7507bff8
xfinger <xfinger@gmail.com>
2015-04-03 12:54:31 -0400

    down to two

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ angular.module('businessTiles', [])
     // {factory:"transloc", cardTitle:"Buses In Service", subtitle:"in service", valueField:"count", method:"getVehicleCount", frequency:"5000", labelField: "call_name"},
     // {factory:"transloc", cardTitle:"Average Bus Speed (mph)", subtitle:"average speed", valueField:"speed", method:"getVehicleCount", frequency:"5000", labelField: "speed"},
      {factory:"cityworks", cardTitle:"Open See Click Fixes", subtitle:"Open in See Click Fix", valueField:"COUNT", method:"getCount", table:"azteca.request", fields:"SRX, SRY, DESCRIPTION", parameters:"initiatedby = 'FIX, SEE CLICK' and not (status in ('CANCEL','CANCEL NOT FOUND', 'CANCEL OTHER', 'CLOSED'))", frequency:"10000", labelField: "DESCRIPTION"},    
-    {factory:"iris", cardTitle:"Permits Issued Today", subtitle:"Permits Issued Today", valueField:"COUNT", method:"getIrisCount", table:"iris.permits_all_view", fields:"NCPIN,GRP_PROPOSED_WORK", parameters:"grp_issue_date>= trunc(sysdate)", frequency:"10000", labelField: "GRP_PROPOSED_WORK"},    
+    //{factory:"iris", cardTitle:"Permits Issued Today", subtitle:"Permits Issued Today", valueField:"COUNT", method:"getIrisCount", table:"iris.permits_all_view", fields:"NCPIN,GRP_PROPOSED_WORK", parameters:"grp_issue_date>= trunc(sysdate)", frequency:"10000", labelField: "GRP_PROPOSED_WORK"},    
     {factory:"sina", cardTitle:"SeeCF Closed ", subtitle:"SCF ClosedToday", valueField:"open", method:"getSinaCount"}    
         
     ];
@@ -146,35 +146,35 @@ angular.module('businessTiles', [])
         return d.promise;
     }
     return service;
-}])
-.factory('transloc', ['$http', '$q', function($http, $q){
-    var baseUrl = 'https://transloc-api-1-2.p.mashape.com/';
-    var headers = {'X-Mashape-Key': 'QcvihLtHdgmshtY0Yjsg7nytW4Iqp1MEy05jsnSqvl1Lqjt9eW'};
-    var service = {};
-    service.getVehicleCount = function () {
-        var d = $q.defer();
-        $http({
-            url: baseUrl + '/vehicles.json',
-            params: {
-                agencies: '20', 
-            },
-            headers: headers
-        }).success(function (data) {
-            var speed = 0;
-            var gj = {
-              type: "FeatureCollection",
-              features: [
-
-              ]
-            };
-
-            angular.forEach(data.data[20], function (v) {
-                speed += v.speed;
-                gj.features.push({type: 'Feature', geometry: {type: 'Point', coordinates:[v.location.lng, v.location.lat]}, properties: {call_name: v.call_name, speed: Math.round(v.speed).toString()}});
-            });
-            d.resolve({count: data.data[20].length, speed: Math.round(speed/data.data[20].length), geojson: gj});
-        });
-        return d.promise;
-    }
-    return service;
 }]);
+// .factory('transloc', ['$http', '$q', function($http, $q){
+//     var baseUrl = 'https://transloc-api-1-2.p.mashape.com/';
+//     var headers = {'X-Mashape-Key': 'QcvihLtHdgmshtY0Yjsg7nytW4Iqp1MEy05jsnSqvl1Lqjt9eW'};
+//     var service = {};
+//     service.getVehicleCount = function () {
+//         var d = $q.defer();
+//         $http({
+//             url: baseUrl + '/vehicles.json',
+//             params: {
+//                 agencies: '20', 
+//             },
+//             headers: headers
+//         }).success(function (data) {
+//             var speed = 0;
+//             var gj = {
+//               type: "FeatureCollection",
+//               features: [
+
+//               ]
+//             };
+
+//             angular.forEach(data.data[20], function (v) {
+//                 speed += v.speed;
+//                 gj.features.push({type: 'Feature', geometry: {type: 'Point', coordinates:[v.location.lng, v.location.lat]}, properties: {call_name: v.call_name, speed: Math.round(v.speed).toString()}});
+//             });
+//             d.resolve({count: data.data[20].length, speed: Math.round(speed/data.data[20].length), geojson: gj});
+//         });
+//         return d.promise;
+//     }
+//     return service;
+// }]);
