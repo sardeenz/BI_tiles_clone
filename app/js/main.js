c58e29615fdb809caaf8ffe98fe8dd6652bbb398
Chad Foley <chad.foley@raleighnc.gov>
2015-04-03 13:38:22 -0400

    integrated node for npm install and npm start to serve this app up

---
+++ app/js/main.js
@@ -0,0 +1,115 @@
+angular.module('businessTiles', [])
+.directive('flipTile', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'flipTile.html',
+        scope: {
+            factory: '=',
+            cardTitle: '=',
+            subtitle: '=',
+            valueField: '=',
+            method: '=',
+            table: '=',
+            fields: '=',
+            parameters: '=',
+            frequency: '=',
+            labelField: '='
+        },
+        controller: function ($scope, $injector, $interval) {
+            getCounts();
+            $interval(getCounts, $scope.frequency);
+           function getCounts () {
+                $injector.get($scope.factory)[$scope.method]($scope.table, $scope.fields, $scope.parameters).then(function (count) {
+                    $scope.value = count[$scope.valueField];
+                    $scope.updated = moment().format('MMMM Do YYYY, h:mm:ss a');
+                    if (count.geojson) {
+                        if (!$scope.geojson) {
+                            $scope.geojson = L.geoJson(count.geojson, {onEachFeature: function (feature, layer) {
+                                if ($scope.labelField) {
+                                    layer.bindLabel(feature.properties[$scope.labelField]);
+                                }
+                            }}).addTo($scope.map);
+                            $scope.map.fitBounds($scope.geojson.getBounds());
+                        }
+                        
+                        $scope.geojson.clearLayers();
+                        $scope.geojson.addData(count.geojson);
+                        
+                    }
+                    
+                });      
+           }
+
+        }, link: function (scope, element, attrs) {
+            scope.map = L.map(element[0].querySelector('.map'));
+            var hour = new Date().getHours();
+            var baseUrl = 'http://{s}.basemaps.cartocdn.com/' + ((hour > 17 || hour < 6) ? 'dark' : 'light') + '_all/{z}/{x}/{y}.png';
+            var tiles = L.tileLayer(baseUrl,{
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+                subdomains: 'abcd',
+                minZoom: 4,
+                maxZoom: 16
+            });
+            scope.map.addLayer(tiles); 
+            scope.map.setView([35.5, -78.8], 12);
+            scope.map.on('load', function(e) {
+                alert('load'); // e is an event object (MouseEvent in this case)
+            });
+        }
+    }
+})
+.controller('businessCtrl', ['$scope', function($scope){
+    $scope.tiles = [
+
+    // {factory:"transloc", cardTitle:"Buses In Service", subtitle:"in service", valueField:"count", method:"getVehicleCount", frequency:"5000", labelField: "call_name"},
+    // {factory:"transloc", cardTitle:"Average Bus Speed (mph)", subtitle:"average speed", valueField:"speed", method:"getVehicleCount", frequency:"5000", labelField: "speed"},
+    {factory:"cityworks", cardTitle:"Open See Click Fixes", subtitle:"Open in See Click Fix", valueField:"COUNT", method:"getCount", table:"azteca.request", fields:"SRX, SRY, DESCRIPTION", parameters:"initiatedby = 'FIX, SEE CLICK' and not (status in ('CANCEL','CANCEL NOT FOUND', 'CANCEL OTHER', 'CLOSED'))", frequency:"10000", labelField: "DESCRIPTION"},    
+    //{factory:"iris", cardTitle:"Permits Issued Today", subtitle:"Permits Issued Today", valueField:"COUNT", method:"getIrisCount", table:"iris.permits_all_view", fields:"NCPIN,GRP_PROPOSED_WORK", parameters:"grp_issue_date>= trunc(sysdate)", frequency:"10000", labelField: "GRP_PROPOSED_WORK"},    
+    {factory:"sina", cardTitle:"SeeCF Closed ", subtitle:"SCF ClosedToday", valueField:"open", method:"getCount"}    
+        
+    ];
+}])
+.factory('cityworks', ['$http', '$q', function($http, $q){
+    var irisUrl = 'http://gisdevarc1/dirt-simple-cwreporting/v1/ws_geo_attributequery.php';
+    var service = {};
+    service.getCount = function (table, fields, params) {
+        var d = $q.defer();
+        $http.jsonp(irisUrl, {
+            params: {
+                table: table,
+                fields: fields,
+                parameters: params,
+                callback: 'JSON_CALLBACK'
+            }
+        }).success(function (data) {
+            var gj = {
+              type: "FeatureCollection",
+              features: [
+
+              ]
+            };
+                                 angular.forEach(data, function (d) {
+                    var ll = spToDD(d.SRX, d.SRY);
+                    ll = [ll[1], ll[0]];
+                    gj.features.push({type: 'Feature', geometry: {type: 'Point', coordinates:ll}, properties: d});
+                });
+            d.resolve({COUNT: data.length, geojson: gj});
+        });
+        return d.promise;
+    }
+    return service;
+}])
+.factory('sina', ['$http', '$q', function($http, $q){
+    var sinaUrl = 'http://rhsoatstapp1:9595/see_click_fix';
+    var service = {};
+    service.getCount = function () {
+        var d = $q.defer();
+        $http.jsonp(sinaUrl).success(function (data) {
+           
+            d.resolve(data.size);
+        });
+        return d.promise;
+    }
+    return service;
+}])
+;
